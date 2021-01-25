e198573700b5df44a0e265928039c241183160b0
Christian <ch.practiques@gmail.com>
2021-01-26 00:29:18 +0100

    Scroll, estilos, fuentes, avances generales

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -9,4 +9,14 @@ export class MainComponent {
   constructor() {
     console.log(navigator.language)
   }
+
+  public scrollTo(location: string) {
+    const main = document.getElementsByClassName('main')[0]
+    const element: any = document.getElementsByClassName(location)[0]
+    const y = element.offsetTop
+    main.scroll({
+      top: y,
+      behavior: 'smooth',
+    })
+  }
 }
